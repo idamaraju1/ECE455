fb7959999cf7db321474019e266b4af1952022d6
Iresh Damaraju <idamaraju@wisc.edu>
2025-10-03 16:20:52 -0500

    p1

---
+++ HW04/hello.cu
@@ -0,0 +1,14 @@
+//Write a CUDA kernel that prints “Hello from thread X” for each GPU thread.
+
+#include <stdio.h>
+    
+__global__ void hello_kernel () {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    printf ( "Hello from thread %d\n", tid );
+}
+
+int main () {
+    hello_kernel <<<2, 4>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
